3710efe4f550e4ebe15da474ee47aea8bda3e6dc
jayathu sanka <shjayathusanka@gmail.com>
2024-12-08 19:42:27 +0530

    stack comit

---
+++ stak/Stack.java
@@ -0,0 +1,42 @@
+package stak;
+
+public class Stack {
+    int stack[] = new int[5]; 
+    int top = 0; 
+
+    public void push(int data) {
+        if (top < stack.length) { 
+            stack[top] = data; 
+            top++; 
+        } else {
+            System.out.println("Stack Overflow"); 
+        }
+
+        //public int pop()
+        // {
+        //     int data;
+        //     top--;
+        //     data = stack[top];
+        //       stack[top] = 0;
+         //     return data;
+        // }
+
+
+
+        //public int peek()
+        // {
+        //     int data;
+        //     data = stack[top-1];
+         //     return data;
+        // }
+
+        
+    }
+
+    public void show() {
+        for (int i = 0; i < top; i++) { 
+            System.out.print(stack[i] + " "); 
+        }
+        System.out.println(); 
+    }
+}
