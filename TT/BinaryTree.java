26b7e96544b703d62a10b3aeb734a25dcadc6b0f
jayathu sanka <shjayathusanka@gmail.com>
2024-12-14 19:36:37 +0530

    in and preorder tree

---
+++ TT/BinaryTree.java
@@ -0,0 +1,55 @@
+package TT;
+
+class Node
+{
+    int data;
+    Node left;
+    Node right;
+
+    public Node(int data){
+        this.data=data;
+    }
+}
+
+public class BinaryTree {
+
+    Node root;
+
+    public void insert(int data){
+
+        root = insertRec(root, data);
+
+        
+    }
+
+    public Node insertRec(Node root, int data){ // Rec means recursion
+
+        if (root==null)
+        root = new Node(data);
+        else if (data < root.data)
+        root.left = insertRec(root.left, data);
+        else if (data > root.data)
+        root.right = insertRec(root.right, data);
+
+
+
+        return root;
+
+    }
+
+    public void preorder()
+    {
+        preorderRec(root);
+
+    }
+    public void preorderRec(Node root)
+    {
+        if(root != null)
+        {
+            System.out.print(root.data + " ");
+            preorderRec(root.left);
+            preorderRec(root.right);
+        }
+    }
+
+}
