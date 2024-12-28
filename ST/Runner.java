c681c4736600bbddee7f4042ecf2c81a743fdd25
jayathu sanka <shjayathusanka@gmail.com>
2024-12-28 11:09:54 +0530

    full and empty ST

--- ST/Runner.java
+++ ST/Runner.java
@@ -4,15 +4,18 @@ public class Runner {
     public static void main(String[] args) {
         
         Stack nums = new Stack();
+       
         nums.push(15);
-        nums.push(8);
-
-        System.out.println(nums.peek());
-
-        nums.push(10);
-
-        System.out.println(nums.pop());
-
         nums.show();
+        nums.push(8);
+        nums.show();
+        nums.push(10);
+        nums.show();
+        
+        System.out.println(nums.pop());
+        System.out.println(nums.pop());
+        System.out.println(nums.pop());
+        System.out.println(nums.size());
+        System.out.println(nums.pop());
     }
 }
--- ST/Stack.java
+++ ST/Stack.java
@@ -6,15 +6,26 @@ public class Stack {
     
     public void push(int data)
     {
+        if(top==5){
+            System.out.println("Stack is full");
+        }else
+        {
         stack[top] = data;
         top++;
+       }
     }
     public int pop(){
-        int data;
+
+        int data = 0;
+        if (isEmpty()) {
+            System.out.println("Stack is empty");
+        }else{
         top--;
         data= stack[top];
         stack[top] = 0 ;
+        }
         return data;
+        
     }
 
     public int peek(){
@@ -23,9 +34,17 @@ public class Stack {
         return data;
     }
 
+    public int size(){
+        return top;
+    }
+    public boolean isEmpty(){
+        return top<=0;
+    }
+
     public void show(){
         for(int n : stack){
             System.out.print(n + " ");
         }
+        System.out.println();
     }
 }
