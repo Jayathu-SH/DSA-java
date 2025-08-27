fe3aa5c63e5c17fd2cf30f9bddcf0c5500b75206
jayathu sanka <shjayathusanka@gmail.com>
2025-08-27 08:28:45 +0530

    Fix comments in ReverseNumber class for clarity and correctness

--- Intervw/ReverseNumber.java
+++ Intervw/ReverseNumber.java
@@ -11,9 +11,9 @@ public class ReverseNumber {
 
         int reversed = 0;
         while (num != 0){
-            int digit = num % 10;
-            reversed = reversed*10 + digit;
-            num = num / 10;
+            int digit = num % 10;//get last digit
+            reversed = reversed*10 + digit;//buid reversed number
+            num = num / 10;//remove last element
         } 
         System.out.println("reversed number : " + reversed);
     }
