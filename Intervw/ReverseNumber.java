dc641a1dc3abce79c0830d5ca9abb2599d335eab
jayathu sanka <shjayathusanka@gmail.com>
2025-09-01 11:30:40 +0530

    Refactor ReverseNumber class to remove user input and set a static number for reversal

--- Intervw/ReverseNumber.java
+++ Intervw/ReverseNumber.java
@@ -1,15 +1,26 @@
 
-import java.util.Scanner;
+//     public static void main(String[] args) {
+        
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter ypur prefer number : ");
+//         int num = sc.nextInt();
 
+//         int reversed = 0;
+//         while (num != 0){
+//             int digit = num % 10;//get last digit
+//             reversed = reversed*10 + digit;//buid reversed number
+//             num = num / 10;//remove last element
+//         } 
+//         System.out.println("reversed number : " + reversed);
+//     }
+// }
 
 public class ReverseNumber {
     public static void main(String[] args) {
         
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter ypur prefer number : ");
-        int num = sc.nextInt();
-
+        int num = 4567;
         int reversed = 0;
+        
         while (num != 0){
             int digit = num % 10;//get last digit
             reversed = reversed*10 + digit;//buid reversed number
@@ -18,3 +29,4 @@ public class ReverseNumber {
         System.out.println("reversed number : " + reversed);
     }
 }
+
