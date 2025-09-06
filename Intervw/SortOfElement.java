36ebec7d1ad07e75a7c83a6f6d34e182ec5df96c
jayathu sanka <shjayathusanka@gmail.com>
2025-09-06 11:19:10 +0530

    Add comment to clarify loop condition in SortOfElement class

--- Intervw/SortOfElement.java
+++ Intervw/SortOfElement.java
@@ -3,7 +3,7 @@ public class SortOfElement {
         int numbers[] = {2,3,4,5,6};
         boolean isSorted = true;
 
-        for (int i = 0; i < numbers.length-1; i++) {
+        for (int i = 0; i < numbers.length-1; i++) {//compare with next element->length-1
             if(numbers[i] > numbers[i+1]){
                 isSorted = false;
                 break;
