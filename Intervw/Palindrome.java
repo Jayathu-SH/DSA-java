9132e335d58cd9cfb5c2a987df82b6afd30734ae
jayathu sanka <shjayathusanka@gmail.com>
2025-09-06 18:52:08 +0530

    Refactor Palindrome class to simplify string reversal using StringBuilder

--- Intervw/Palindrome.java
+++ Intervw/Palindrome.java
@@ -1,27 +1,27 @@
-public class Palindrome{
-    public static void main(String[] args){
-        String str = "madam";
-        String rev = "";
-        
-        
-        for(int i=str.length()-1;i>=0;i--){
-            rev+=str.charAt(i);
-            
-        }
-            boolean equal = str.equals(rev);
-        
-        System.out.println(equal);
-        
-    }
-}
 // public class Palindrome{
 //     public static void main(String[] args){
 //         String str = "madam";
-//         String rev = new StringBuilder(str).reverse().toString();
-//         System.out.println(str.equals(rev));
+//         String rev = "";
+        
+        
+//         for(int i=str.length()-1;i>=0;i--){
+//             rev+=str.charAt(i);
+            
+//         }
+//             boolean equal = str.equals(rev);
+        
+//         System.out.println(equal);
         
 //     }
 // }
+public class Palindrome{
+    public static void main(String[] args){
+        String str = "madam";
+        String rev = new StringBuilder(str).reverse().toString();
+        System.out.println(str.equals(rev));
+        
+    }
+}
 
 
 // StringBuilder: single-thread
